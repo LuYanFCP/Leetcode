55aeddb4131364fa2644e670702b0b865db389eb
luyanfcp <lyhxbq@gmail.com>
2019-09-23 21:39:37 +0800

    change

--- 10.cpp
+++ 10.cpp
@@ -14,15 +14,19 @@ using std::max;
 
 class Solution {
 private:
-    int **ptr;
-    int len;
+    // int **ptr;
+    // int len;
+    int *ptr;
     
 public:
     void Clear() {
-        for (int i = 0; i < len; i++) {
-            delete[] ptr[i];
+        // for (int i = 0; i < len; i++) {
+        //     delete[] ptr[i];
+        // }
+        // delete[] ptr;
+        if (!ptr) {
+            delete[] ptr;
         }
-        delete[] ptr;
     }
     ~Solution() {
         Clear();
@@ -39,26 +43,43 @@ public:
         int rs = 0;
 
         // 初始化二维数组
-        len = n;
-        ptr = new int*[n];
-        for (int i = 0; i < n; i++) {
-            ptr[i] = new int[n];
+        // len = n;
+        // ptr = new int*[n];
+        // for (int i = 0; i < n; i++) {
+        //     ptr[i] = new int[n];
+        // }
+        ptr = new int[n]();
+
+        // // 遍历数组进行计算   
+        // for (int i = 1; i < n; i++) {
+        //     for (int j = n - 1; j >=0; j--) {
+        //         // if (j != 0) { 
+        //         //     ptr[i][j] = max( max(ptr[i - 1][j], ptr[i][j - 1]), min(height[i], height[j]) * (i - j));
+        //         // } else {
+        //         //     ptr[i][j] = max(ptr[i - 1][j], (min(height[i], height[j]) * (i - j)));
+        //         // }
+        //         // cout << "{i: " << i << ", j:" << j << "}" << endl;
+        //         cout << ptr[j] << endl;
+        //         if (j != 0)
+        //             ptr[j] = max(max (ptr[j], ptr[j - 1]), min(height[i], height[j]) * (i - j));
+        //         else
+        //             ptr[j] = max(ptr[j], min(height[i], height[j]) * (i - j));
+        //         if (ptr[j] > rs)
+        //             rs = ptr[j];
+        //         // if (ptr[i][j] > rs) {
+        //         //     rs = ptr[i][j];
+        //         // }
+        //     }
+        // }      
+
+	
+		int p = 0,q = height.size() - 1;
+		int ans=INT_MIN;
+		while(p < q) {
+			ans=max(ans, min(height[p],height[q]) * (q - p));
+			if(height[p]<height[q]) p++;
+			else q--;
         }
-        
-        // 遍历数组进行计算   
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (j != 0) { 
-                    ptr[i][j] = max( max(ptr[i - 1][j], ptr[i][j - 1]), min(height[i], height[j]) * (i - j));
-                } else {
-                    ptr[i][j] = max(ptr[i - 1][j], (min(height[i], height[j]) * (i - j)));
-                }
-                
-                if (ptr[i][j] > rs) {
-                    rs = ptr[i][j];
-                }
-            }
-        }      
         return rs;
 
     }
