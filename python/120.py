8754ca79b4ce84d1be4f699d7ed7de9446a533bb
luyanfcp <luyanfcp@example.com>
2020-02-26 20:26:33 +0800

    增加 时间和内存

--- python/120.py
+++ python/120.py
@@ -1,5 +1,9 @@
 class Solution:
     def minimumTotal(self, triangle):
+        """
+        56 ms, faster than 86.07% 
+        13.6 MB, less than 33.33% 
+        """
         length = len(triangle)
         lst = triangle[length - 1][:]  # 滚动数组
         for i in range(length - 2, -1, -1):
